1b7e0b86b7251b63526a8bba911efeb22d96997c
FelixxMerc <128545373+FelixxMerc@users.noreply.github.com>
2023-05-14 13:33:19 -0400

    Update Collection.sol

--- Collection.sol
+++ Collection.sol
@@ -14,7 +14,7 @@ contract Collection is ERC721Enumerable, Ownable {
     using Strings for uint256;
     string public baseURI;
     string public baseExtension = ".json";
-    uint256 public maxSupply = 100000;
+    uint256 public maxSupply = 10000;
     uint256 public maxMintAmount = 5;
     bool public paused = false;
 
